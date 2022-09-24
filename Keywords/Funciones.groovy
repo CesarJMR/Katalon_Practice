9369d3f32b7dd8c15ca9716c282517cec0e3ea38
cesar <cesar@CesarJose>
2022-09-24 02:18:45 -0400

    Actualizando

--- Keywords/Funciones.groovy
+++ Keywords/Funciones.groovy
@@ -19,30 +19,30 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable
 
 public class Funciones {
-	
+
 	@Keyword
 	def Entrar_a_web_app() {
 		WebUI.openBrowser(findTestData('AutomationPractice').getValue(1, 1))
-		
+
 		WebUI.maximizeWindow()
-		
+
 		WebUI.waitForPageLoad(30)
 	}
 
 	@Keyword
 	def Log_in() {
 		WebUI.click(findTestObject('Btn_Sign_in'))
-		
+
 		WebUI.waitForElementPresent(findTestObject('Escenario3/Login_form'), 30)
-		
+
 		WebUI.setText(findTestObject('Escenario3/Email'), findTestData('AutomationPractice').getValue(3, 1))
-		
+
 		WebUI.setText(findTestObject('Escenario3/Password'), findTestData('AutomationPractice').getValue(4, 4))
-		
+
 		WebUI.click(findTestObject('Escenario3/Btn_Log_in'))
-		
+
 		WebUI.waitForPageLoad(30)
-		
+
 		WebUI.verifyTextPresent(findTestData('AutomationPractice').getValue(4, 9), false)
 	}
 }
